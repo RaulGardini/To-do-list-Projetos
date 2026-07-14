f5e347489e0217cfde01c30aab3b389888a2eb63
RaulGardini <rap.gardini@gmail.com>
2026-07-14 11:29:24 -0300

    feat:melhorando api de tarefas

--- Services/TarefasProjeto/IServiceTarefaProjeto.cs
+++ Services/TarefasProjeto/IServiceTarefaProjeto.cs
@@ -1,12 +1,12 @@
-﻿using ToDoList.DTOs.TarefasProjeto;
+using ToDoList.DTOs.TarefasProjeto;
 
 namespace ToDoList.Services.TarefasProjeto;
 
 public interface IServiceTarefaProjeto
 {
-    Task<IEnumerable<ReadTarefaProjetoDTO>> GetAllByProjetoAsync(int projetoId);
-    Task<ReadTarefaProjetoDTO?> GetByIdAsync(int tarefaId);
-    Task<ReadTarefaProjetoDTO> CreateAsync(CreateTarefaProjetoDTO request);
-    Task UpdateAsync(int tarefaId, UpdateTarefaProjetoDTO request);
-    Task DeleteAsync(int tarefaId);
+    Task<IEnumerable<ReadTarefaProjetoDTO>> GetAllByProjetoAsync(int projetoId, Guid usuarioId);
+    Task<ReadTarefaProjetoDTO?> GetByIdAsync(int tarefaId, Guid usuarioId);
+    Task<ReadTarefaProjetoDTO> CreateAsync(CreateTarefaProjetoDTO request, Guid usuarioId);
+    Task UpdateAsync(int tarefaId, UpdateTarefaProjetoDTO request, Guid usuarioId);
+    Task DeleteAsync(int tarefaId, Guid usuarioId);
 }
